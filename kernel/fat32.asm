98d6bfd1f79e989bb61aa846310058e76584a856
SopaXorzTaker <markboldyrev@gmail.com>
2018-11-26 17:21:17 +0200

    FAT32: first steps towards an actual implementation.

--- kernel/fat32.asm
+++ kernel/fat32.asm
@@ -11,6 +11,52 @@ struc partition
     .size       resd 1
 endstruc
 
+; FAT32 BPB
+struc FATBPB
+    .jump               resb 3
+    .oemId              resb 8
+    .wBytesPerSector    resw 1
+    .bSectorsPerCluster resb 1
+    .wReservedSectors   resw 1
+    .bNumFATs           resb 1
+    .wNumRootEntries    resw 1
+    .wTotalSectors      resw 1
+    .bMediaDescriptor   resb 1
+    .wSectorsPerFAT     resw 1
+    .wSectorsPerTrack   resw 1
+    .wNumHeads          resw 1
+    .dHiddenSectors     resd 1
+    .dTotalSectors      resd 1
+
+    .dSectorsPerFAT     resd 1
+    .wFlags             resw 1
+    .wFATVersion        resw 1
+    .dRootCluster       resd 1
+    .wFSInfoSector      resw 1
+    .wBackupBootSector  resw 1
+    .reserved           resb 12
+    .bDriveNumber       resb 1
+    .bWinNTFlags        resb 1
+    .bSignature         resb 1
+    .dVolumeId          resd 1
+    .volumeLabel        resb 11
+    .systemId           resb 8
+endstruc
+
+
+; FAT32 context
+struc FATContext
+    .driveNumber        resb 1
+    .sectorsPerCluster  resb 1
+    .bytesPerCluster    resw 1
+    .FATOffset          resd 1
+    .dataOffset         resd 1
+    .dirsPerCluster     resd 1
+    .rootCluster        resd 1
+    .size:
+endstruc
+
+
 ; Disk Address Packet
 fat32._dap:
     .size       db 16
@@ -24,7 +70,7 @@ fat32._dap:
 
 ; Reads the EAX'th sector from the drive DL into the memory at FS:0000.
 ; Returns with the carry set if the read was unsuccessful.
-fat32.readSector:
+fat32._readSector:
     push eax
     push si
 
@@ -47,9 +93,9 @@ fat32.readSector:
 
 ; Checks if the DH'th partition on drive DL contains a FAT32 filesystem.
 ; Returns with the carry flag set on error.
-; ZF is set if the partition type matches, clear otherwise.
-fat32.checkPartition:
-    push eax
+; EAX will contain the starting LBA of the partition if it's found, zero otherwise.
+; CF will be set if either the allocation or the read has failed.
+fat32.readPartition:
     push bx
     push cx
     push fs
@@ -63,7 +109,7 @@ fat32.checkPartition:
 
     ; Read the first sector
     xor eax, eax
-    call fat32.readSector
+    call fat32._readSector
     jc .error
 
     ; Offset into the MBR
@@ -77,17 +123,18 @@ fat32.checkPartition:
     add bx, 0x1be
 
     ; Read the system identifier of the partition.
-    mov al, [fs:bx+partition.systemId]
+    mov cl, [fs:bx+partition.systemId]
 
 .done:
+    ; EAX contains the starting LBA of the partition.
+    mov eax, [fs:bx+partition.startLBA]
+
     ; Free the buffer
     call kalloc.kfree
 
-    ; Is the partition type FAT32 (set ZF).
-    cmp al, 0x0c
-
-    ; No carry
-    clc
+    ; Is the partition type FAT32?
+    cmp cl, 0x0c
+    jnz .wrongType
 
     jmp .return
 
@@ -96,11 +143,227 @@ fat32.checkPartition:
     call kalloc.kfree
 
 .errorNoFree:
+    ; Set carry
+    stc
+
+.return:
+    ; Set ZF according to EAX.
+    or eax, eax
+
+    pop fs
+    pop cx
+    pop bx
+    ret
+
+.wrongType:
+    ; No carry, EAX=0
+    xor eax, eax
+    jmp .return
+
+
+; Allocates a FAT32 context structure.
+; GS will point to the allocated structure on success, CF will be set otherwise.
+fat32.context.alloc:
+    push cx
+    push fs
+
+    ; Try allocating memory
+    mov cx, FATContext.size
+    call kalloc.kalloc
+
+    ; Return with carry
+    jc .return
+
+    ; Store FS in GS
+    push fs
+    pop gs
+
+.done:
+    ; No carry
+    clc
+
+.return:
+    ; Restore FS
+    pop fs
+    pop cx
+    ret
+
+
+; Frees a FAT32 context structure.
+; GS should point to the allocated structure.
+fat32.context.free:
+    ; Preserve FS
+    push fs
+
+    ; Free the memory
+    push gs
+    pop fs
+    call kalloc.kfree
+
+    ; Restore FS
+    pop fs
+    ret
+
+
+; Mounts a FAT32 filesystem.
+; DL is the drive number and EAX is the starting LBA of the partition,
+;  returned by fat32.readPartition.
+; GS points to the FATContext structure that will be filled.
+; CF is set on error.
+fat32.mount:
+    push eax
+    push ebx
+    push edx
+    push cx
+    push fs
+
+    ; Allocate memory for the first sector of the partition
+    mov cx, 512
+    call kalloc.kalloc
+
+    ; Allocation error (don't free memory)
+    jc .errorNoFree
+
+    ; Read the first sector of the partition
+    call fat32._readSector
+    jc .error
+
+    ; Store the drive number
+    mov [gs:FATContext.driveNumber], dl
+
+    ; Store the number of sectors per cluster
+    mov al, [fs:FATBPB.bSectorsPerCluster]
+    mov [gs:FATContext.sectorsPerCluster], al
+
+    ; Find the FAT offset
+    mov eax, [fs:FATBPB.dHiddenSectors]
+
+    ; Add the number of reserved sectors
+    xor ebx, ebx
+    mov bx, [fs:FATBPB.wReservedSectors]
+    add eax, ebx
+
+    ; Store the value
+    mov [gs:FATContext.FATOffset], eax
+    mov ebx, eax
+
+    ; Add the FAT lengths (CX * sectorsPerFAT)
+    mov eax, [fs:FATBPB.dSectorsPerFAT]
+    xor cx, cx
+    mov cl, [fs:FATBPB.bNumFATs]
+
+    ; EAX*CX -> EDX:EAX
+    mul cx
+
+    ; Add the previous offset
+    add eax, ebx
+
+    ; Store the value
+    mov [gs:FATContext.dataOffset], eax
+
+    ; Copy the root cluster number
+    mov eax, [fs:FATBPB.dRootCluster]
+    mov [gs:FATContext.rootCluster], eax
+
+    ; Find the number of directory entries per cluster
+    mov ax, [fs:FATBPB.bSectorsPerCluster]
+
+    ; Multiply by 16 (directory entries per sector)
+    mov cx, ax
+    shl ax, 4
+
+    ; Store
+    mov [gs:FATContext.dirsPerCluster], ax
+
+    ; Find the amount of bytes per cluster.
+    ; Multiply CX (sectors per cluster) by 512 (the sector size)
+    shl cx, 9
+
+    ; Store
+    mov [gs:FATContext.bytesPerCluster], cx
+
+    ; Done
+    clc
+    jmp .return
+
+.error:
+    ; Free the memory allocated
+    call kalloc.kfree
+
+.errorNoFree:
     stc
 
 .return:
     pop fs
     pop cx
-    pop bx
+    pop edx
+    pop ebx
     pop eax
     ret
+
+
+; Reads a FAT entry for a given cluster.
+; EAX contains the cluster number, GS points to the FATContext structure.
+; Returns with CF set on error, otherwise EAX will contain the value read or 0 if the cluster is EOF.
+fat32.readFAT:
+    push bx
+    push cx
+    push dx
+    push fs
+
+    ; Allocate a sector for reading the FAT into (FS will point there).
+    mov cx, 512
+    call kalloc.kalloc
+    jc .errorNoFree
+
+    ; Find the correct sector of the FAT and the offset into that sector.
+
+    ; Multiply EAX by 4 (the size of a FAT32 entry) and divide it by 512 (the size of a sector).
+    shl eax, 2
+
+    ; BX is the remainder
+    mov bx, ax
+    and bx, 0x1ff
+
+    ; Divide by 512
+    shr eax, 9
+
+    ; Add the FAT offset
+    add eax, [gs:FATContext.FATOffset]
+
+    ; Read the sector
+    mov dl, [gs:FATContext.driveNumber]
+    call fat32._readSector
+    jc .error
+
+    ; EAX contains the correct entry now.
+    mov eax, [fs:bx]
+    and eax, 0x0fffffff
+
+    ; Is it the EOF value?
+    cmp eax, 0x0ffffff8
+    jnae .done
+
+    ; EOF, clear EAX
+    xor eax, eax
+
+.done:
+    ; Clear carry, no error.
+    clc
+
+.return:
+    ; Restore the registers.
+    pop fs
+    pop dx
+    pop cx
+    pop bx
+    ret
+
+.error:
+    ; Free the memory we allocated.
+    call kalloc.kfree
+
+.errorNoFree:
+    ; Set carry, done.
+    stc
+    jmp .return
--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -8,6 +8,7 @@ jmp init
 ; Include the subroutines
 %include "console.asm"
 %include "kalloc.asm"
+%include "fat32.asm"
 
 ; Boot status
 bootStatus:
@@ -17,6 +18,7 @@ init:
     ; Initialize the segments
     mov ax, cs
     mov ds, ax
+    mov es, ax
 
     ; Preserve the drive number
     mov [bootStatus.driveNumber], dl
@@ -27,15 +29,94 @@ init:
     ; Clear the screen
     call console.clearScreen
 
-    xor edx, edx
-    mov dl, [bootStatus.driveNumber]
+    ; Print the boot message
+    mov si, bootMessage
+    call console.print
+
+.mount:
+    ; Try allocating a context structure for working with FAT
+    call fat32.context.alloc
+    jc error
+
+    ; Print the mount message
+    mov si, mountMessage
+    call console.print
+
+    ; DL already contains the drive number we want, clear DH.
+    xor dh, dh
+
+.nextPart:
+    ; Log
+    mov si, readPartMessage
+    call console.print
+
+    ; Read the partition entry DH on drive DL.
+    call fat32.readPartition
+    jc error
+    jz .skip
+
+    ; Found!
+    ; Preserve EDX
+    mov ebx, edx
+
+    ; Copy the LBA number into EDX for printf.
+    mov edx, eax
+    mov si, partFoundMessage
+    call console.printf
+
+    ; Restore EDX
+    mov edx, ebx
+
+    ; Try mounting the partition
+    call fat32.mount
+    jc .mountError
+
+    ; Done, test
+    mov eax, [fs:FATContext.rootCluster]
+    call fat32.readFAT
+    jc error
+
+    mov edx, eax
     mov si, testMessage
     call console.printf
+    jmp halt
+
+    ; Next?
+.skip:
+    inc dh
+    cmp dh, 4
+    jnz .nextPart
+
+.notFound:
+    mov si, partNotFoundMessage
+    call console.print
+    jmp halt
+
+.mountError:
+    mov si, mountErrorMessage
+    call console.print
+    jmp halt
+
 
 halt:
     ; Wait for an interrupt, halt again
     hlt
     jmp halt
 
+error:
+    ; Print the error message, halt
+    mov si, errorMessage
+    call console.print
+    jmp halt
 
-testMessage db "Hello world from YaxOS! Drive number: %x.", 13, 10, 0
+
+bootMessage db "[+] kernel: booted!", 13, 10, 0
+
+errorMessage db "[!] kernel: error, halting...", 13, 10, 0
+mountMessage db "[+] kernel: looking for a partition...", 13, 10, 0
+readPartMessage db "[.] kernel: reading partition", 13, 10, 0
+partNotFoundMessage db "[!] kernel: no FAT32 partitions found!", 13, 10, 0
+partFoundMessage db "[+] kernel: found a FAT32 partition at LBA %x, mounting...", 13, 10, 0
+mountErrorMessage db "[!] kernel: couldn't mount the partition!", 13, 10, 0
+
+testMessage db "readFAT test: %x.", 13, 10, 0
